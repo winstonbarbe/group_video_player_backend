39cfcb9272261de8c929bb4f5baf0393930c2515
winstonbarbe <winston.barbe@gmail.com>
2021-02-28 20:06:55 -0500

    updates accisiation in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   
-  has_many :videos
+  has_many :videos, -> { order(updated_at: :desc) }
 end
